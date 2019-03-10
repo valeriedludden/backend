6509c323184e03c84a129a04bc6185c56e70378e
Valerie <valleygirl021@gmail.com>
2019-03-09 17:41:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require ('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+let app = express();
+let userArray = [];
+let editUser;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// needs to be set as 'views'
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//App GETS
+app.get('/', (req, res)=>{
+    res.render('index', {title: 'Add users'});
+});
+
+app.get('/users', (req, res)=>{
+    editUser = '';
+    res.render('users', {title: 'User listing', userArray: userArray})
+});
+
+app.get('/edit/:id', (req, res)=>{
+    userArray.forEach( user =>{
+        if(user.userId === req.params.id){
+            editUser = user;
+            console.log("Get - Edit user name" + editUser.userName);
+        }
+        console.log(" Get - User name" + user.userName);
+    });
+
+    res.render('edit', {title: 'Edit User', user: editUser })
+});
+
+app.get('/delete/:id', (req, res)=>{
+    editUser = req.params.id;
+    userArray = userArray.filter(user =>{
+       return user.userId !== editUser});
+    res.redirect('/users');
+});
+
+//App POSTS
+
+app.post('/', (req, res)=>{
+
+   let user = {
+       userId: req.body.userId,
+       userName: req.body.username,
+       email: req.body.email,
+       address: req.body.address,
+       age: req.body.age
+   };
+   userArray.push(user);
+   res.redirect('/users');
+});
+
+app.post('/edit/:id', (req, res)=>{
+ userArray.forEach( user =>{
+     console.log("is this working?");
+     if (user.userName === editUser.userName){
+         user.userId = req.body.userId;
+         user.userName = req.body.username;
+         user.email = req.body.email;
+         user.address = req.body.address;
+         user.age = req.body.age;
+         console.log("hello")
+     }
+     console.log("Post - User name" + user.userName);
+     console.log("Post - Edit user name" + editUser.userName);
+ });
+    console.log("Post - user array " + userArray);
+    res.redirect('/');
+});
+
+//Set up port
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000');
+});
